2898ebcf6b1937eb52286f85430ec4bdb160a9b0
LuoJie <1299124551@qq.com>
2022-01-13 14:24:21 +0800

    自定义组件的使用

---
+++ component/NavHeader/NavHeader.js
@@ -0,0 +1,30 @@
+// component/NavHeader/NavHeader.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        title: {
+            type: String,
+            value: '请传对应的值'
+        },
+        nav: {
+            type: String,
+            value: '请传对应的值'
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
