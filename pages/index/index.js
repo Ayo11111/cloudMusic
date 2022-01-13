5b7e42225222e1bb17b934a0872d1c2d079d6887
LuoJie <1299124551@qq.com>
2022-01-13 18:38:41 +0800

    完成排行榜

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,30 +7,45 @@ Page({
      * 页面的初始数据
      */
     data: {
-        banners: [],
-        recommendSongs: []
+        banners: [], //轮播图数据
+        recommendSongs: [], //推荐歌曲数据
+        topList: [] //排行榜数据
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad:  function () {
+    onLoad: function () {
         this.requestFun()
     },
 
-     // 发送请求的接口
-    async requestFun(){
-          // 轮播图请求接口
-          let { banners } = await request('/banner', { type: 2 })
-          this.setData({
-              banners: banners
-          })
-  
-          // 获取推荐歌单数据
-          let { result } = await request('/personalized', { limit: 20 })
-          this.setData({
-              recommendSongs: result
-          })
+    // 发送请求的接口
+    async requestFun() {
+        // 轮播图请求接口
+        let { banners } = await request('/banner', { type: 2 })
+        this.setData({
+            banners: banners
+        })
+
+        // 获取推荐歌单数据
+        let { result } = await request('/personalized', { limit: 20 })
+        this.setData({
+            recommendSongs: result
+        })
+
+        // 获取排行榜数据
+        let list = []
+        for (let index = 0; index < 5; index++) {
+            let { playlist } = await request('/top/list', { idx: index })
+            let playListItem = { name: playlist.name, tracks: playlist.tracks.slice(0, 3) }
+            list.push(playListItem)
+
+            // 放在里面就不会长时间白屏，不过会牺牲性能多次渲染
+            this.setData({
+                topList: list
+            })
+        }
+       
     },
 
     /**
