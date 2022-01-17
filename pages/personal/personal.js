eae1b9a02c22c78b62287e7a220ad5020d0efaa0
LuoJie <1299124551@qq.com>
2022-01-17 10:14:36 +0800

    个人中心交互

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,11 +1,14 @@
-// pages/personal/personal.js
+let startY = 0 // 手指起始的坐标
+let moveY = 0  // 手指移动的坐标 
+let moveDistance = 0 //手指移动的距离
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        coverTransform: 'translateY(0)',
+        coveTransition: ''
     },
 
     /**
@@ -13,6 +16,41 @@ Page({
      */
     onLoad: function (options) {
 
+    },
+
+    // 手指点击时的回调
+    handleTouchStart(event) {
+        this.setData({
+            coveTransition:''
+        })
+        // 获取手指的起始坐标
+        startY = event.touches[0].clientY
+    },
+
+    // 手指移动时的回调
+    handleTouchMove(event) {
+        moveY = event.touches[0].clientY
+        moveDistance = moveY - startY
+
+        if (moveDistance <= 0) {
+            return
+        }
+
+        if (moveDistance >= 80) {
+            moveDistance = 80
+        }
+
+        this.setData({
+            coverTransform: `translateY(${moveDistance}rpx)`
+        })
+    },
+
+    // 手指离开时的回调
+    handleTouchEnd() {
+        this.setData({
+            coverTransform: 'translateY(0)',
+            coveTransition: 'transform 1s linear'
+        })
     },
 
     /**
