1c6f4b1ec652676e5be484300577191f538c53f6
Ayo11111 <1299124551@qq.com>
2022-04-05 21:24:20 +0800

    增加热搜榜已经模糊搜索功能的实现

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,3 +1,5 @@
+import request from '../../utils/reques'
+let isSend = false // 函数节流使用
 // pages/search/search.js
 Page({
 
@@ -5,14 +7,59 @@ Page({
    * 页面的初始数据
    */
   data: {
+    placeholderContent: '',
+    hotList: [],
+    sendData: '', // 搜索框发送接口数据
+    searchList: [],
+  },
 
+  // 初始化placeholder数据
+  async getInitPlaceholder() {
+    const placeholderData = await request('/search/default')
+    const hotListData = await request('/search/hot/detail')
+    console.log(hotListData);
+    this.setData({
+      placeholderContent: placeholderData.data.showKeyword,
+      hotList: hotListData.data
+    })
+  },
+
+  // 搜索框事件回调
+  inputHandle(e) {
+    this.setData({
+      sendData: e.detail.value
+    })
+
+    if (!this.data.sendData) {
+      this.setData({
+        searchList: []
+      })
+      return
+    }
+
+    if (isSend) {
+      return
+    }
+    this.getSearchList()
+    isSend = true
+    this.setTimeoutId = setTimeout(() => {
+      isSend = false
+    }, 500)
+  },
+
+  // 搜索框模糊搜索接口请求
+  async getSearchList() {
+    const searchListData = await request('/search', { keywords: this.data.sendData, limit: 10 })
+    this.setData({
+      searchList: searchListData.result.songs
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getInitPlaceholder()
   },
 
   /**
